828fc49ff484f74c2c15e7195561eb43f3debfd1
LucaFalasca <30274870+LucaFalasca@users.noreply.github.com>
2021-01-08 15:07:27 +0100

    test travis 2

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,10 +3,14 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	TextField t;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
